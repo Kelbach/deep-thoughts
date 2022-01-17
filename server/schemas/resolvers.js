a4e7ec1d125c91dc5c9cf6c6431aabaa078273b1
kelbach <87092340+Kelbach@users.noreply.github.com>
2022-01-17 14:30:37 -0500

    21.1.5 thoughts, users, reactions

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,8 +1,11 @@
+const { User, Thought } = require('../models');
+
 const resolvers = {
     Query: {
-      helloWorld: () => {
-        return 'Hello world!';
-      }
+        thoughts: async (parent, { username }) => {
+            const params = username ? { username } : {};
+            return Thought.find(params).sort({ createdAt: -1 });
+        },
     }
 };
   
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,8 +3,24 @@ const { gql } = require('apollo-server-express');
 
 // create our typeDefs
 const typeDefs = gql`
+  type Thought {
+    _id: ID
+    thoughtText: String
+    createdAt: String
+    username: String
+    reactionCount: Int
+    reactions: [Reaction]
+  }
+
+  type Reaction {
+    _id: ID
+    reactionBody: String
+    createdAt: String
+    username: String
+  }
+
   type Query {
-    helloWorld: String
+    thoughts(username: String): [Thought]
   }
 `;
 
