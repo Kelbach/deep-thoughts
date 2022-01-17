1025aadbc55d90a0f7335b0e31a1bd08bd401665
kelbach <87092340+Kelbach@users.noreply.github.com>
2022-01-17 16:20:23 -0500

    21.2.6 just sending it. addThought, addReaction, addFriend

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -62,6 +62,49 @@ const resolvers = {
           
             const token = signToken(user);
             return { token, user };
+        },
+        addThought: async (parent, args, context) => {
+            // context.user checks for login token
+            if (context.user) {
+              const thought = await Thought.create({ ...args, username: context.user.username });
+          
+              await User.findByIdAndUpdate(
+                { _id: context.user._id },
+                { $push: { thoughts: thought._id } },
+                // new: true returns updated user doc
+                { new: true }
+              );
+          
+              return thought;
+            }
+          
+            throw new AuthenticationError('You need to be logged in!');
+        },
+        addReaction: async (parent, { thoughtId, reactionBody }, context) => {
+            if (context.user) {
+              const updatedThought = await Thought.findOneAndUpdate(
+                { _id: thoughtId },
+                { $push: { reactions: { reactionBody, username: context.user.username } } },
+                { new: true, runValidators: true }
+              );
+          
+              return updatedThought;
+            }
+          
+            throw new AuthenticationError('You need to be logged in!');
+        },
+        addFriend: async (parent, { friendId }, context) => {
+            if (context.user) {
+              const updatedUser = await User.findOneAndUpdate(
+                { _id: context.user._id },
+                { $addToSet: { friends: friendId } },
+                { new: true }
+              ).populate('friends');
+          
+              return updatedUser;
+            }
+          
+            throw new AuthenticationError('You need to be logged in!');
         }
     }
 };
