0b6c1fb512c71445ef6a397c43fed3d3cbec0a52
kelbach <87092340+Kelbach@users.noreply.github.com>
2022-01-17 15:34:07 -0500

    21.2.3 addUser, login mutations

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,4 +1,5 @@
 const { User, Thought } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
 
 const resolvers = {
     Query: {
@@ -23,6 +24,28 @@ const resolvers = {
         // place this inside of the `Query` nested object right after `thoughts` 
         thought: async (parent, { _id }) => {
             return Thought.findOne({ _id });
+        }
+    },
+    Mutation: {
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+          
+            return user;
+        },
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+          
+            if (!user) {
+              throw new AuthenticationError('Incorrect credentials');
+            }
+          
+            const correctPw = await user.isCorrectPassword(password);
+          
+            if (!correctPw) {
+              throw new AuthenticationError('Incorrect credentials');
+            }
+          
+            return user;
         }
     }
 };
