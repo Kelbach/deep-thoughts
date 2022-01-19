0c05735e101614068fd4f312b1aff1c7523c4873
kelbach <87092340+Kelbach@users.noreply.github.com>
2022-01-19 15:58:56 -0500

    21.6.3 AddFriend fn to mutations and Profile.js

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Redirect, useParams } from 'react-router-dom';
 import FriendList from '../components/FriendList';
 import ThoughtList from '../components/ThoughtList';
-import { useQuery } from '@apollo/client';
+import { ADD_FRIEND } from '../utils/mutations';
+import { useQuery, useMutation } from '@apollo/client';
 import { QUERY_USER, QUERY_ME } from '../utils/queries';
 import Auth from '../utils/auth';
 
 const Profile = () => {
   const { username: userParam } = useParams();
+  const [addFriend] = useMutation(ADD_FRIEND);
 
   const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
     variables: { username: userParam }
@@ -32,12 +34,28 @@ const Profile = () => {
     );
   }
 
+  const handleClick = async () => {
+    try {
+      await addFriend({
+        variables: { id: user._id }
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
   return (
     <div>
       <div className="flex-row mb-3">
-      <h2 className="bg-dark text-secondary p-3 display-inline-block">
-        Viewing {userParam ? `${user.username}'s` : 'your'} profile.
-      </h2>
+        <h2 className="bg-dark text-secondary p-3 display-inline-block">
+          Viewing {userParam ? `${user.username}'s` : 'your'} profile.
+        </h2>
+
+        {userParam && (
+          <button className="btn ml-auto" onClick={handleClick}>
+            Add Friend
+          </button>
+        )}
       </div>
 
       <div className="flex-row justify-space-between mb-3">
